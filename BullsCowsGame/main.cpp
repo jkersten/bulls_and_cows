6971a0d127c7bf480e5ea2ce3f6c8f184a8ca925
Joost Kersten <joost@salonized.com>
2018-06-06 10:16:27 +0200

    Initial Commit

---
+++ BullsCowsGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullsCowsGame
+//
+//  Created by Joost Kersten on 06/06/2018.
+//  Copyright © 2018 Joost Kersten. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
